44f3f9a2849112f0d898e0dd984af171183c05bd
Tran Vo <vobaohoangtran@gmail.com>
2025-10-27 21:04:27 -0400

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Please feel free to change the JS as you see fit! This is just a starting point.
-
-const root = document.getElementById("root");
-root.addEventListener("click", (event) => {
-  console.log(event.target.tagName);
-  console.log(event.target);
-});
